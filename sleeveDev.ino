c98f12878260944472f37bdecbbf64123e8879ef
hantar <hantar@hawk.iit.edu>
2017-03-06 21:14:08 -0600

    proof of concept code - fixed formatting

--- sleeveDev.ino
+++ sleeveDev.ino
@@ -1,11 +1,13 @@
+// Libraries
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_LSM9DS0.h>
 #include <Adafruit_Sensor.h>
 
-// i2c
+// i2c (accelerometer/compass/gyro)
 Adafruit_LSM9DS0 lsm = Adafruit_LSM9DS0();
 
+// Variables
 const int flexPin = A0;
 const int vibPin = 3;
 int bendVal = 0;
@@ -17,13 +19,11 @@ void setupLSM() {
   //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
   //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
   //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
-
   // 2.) Set the magnetometer sensitivity
   lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
   //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_4GAUSS);
   //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_8GAUSS);
   //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
-
   // 3.) Setup the gyroscope
   lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
   //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
@@ -44,7 +44,6 @@ void setup() {
   Serial.println("");
   Serial.println("");
 }
-
 void loop() {
   bendVal = analogRead(flexPin); // Send Flex value to bendVal
   Serial.println(bendVal); // Output value
@@ -58,18 +57,18 @@ void loop() {
     digitalWrite(vibPin, LOW); // Stop vibration
     Serial.print("Degrees: "); Serial.print(bendVal); Serial.write(176); Serial.println(""); // Displays degree value
   }
-  bendVal = bendVal + 1;
-  lsm.read();
+  //bendVal = bendVal + 1;
+  lsm.read(); // grab data off i2c
   //Display grouping of data
-  Serial.print("Accel X: "); Serial.print((int)lsm.accelData.x); Serial.println(" ");
-  Serial.print("Y: "); Serial.print((int)lsm.accelData.y);       Serial.println(" ");
-  Serial.print("Z: "); Serial.println((int)lsm.accelData.z);     Serial.println(" ");
-  Serial.print("Mag X: "); Serial.print((int)lsm.magData.x);     Serial.println(" ");
-  Serial.print("Y: "); Serial.print((int)lsm.magData.y);         Serial.println(" ");
-  Serial.print("Z: "); Serial.println((int)lsm.magData.z);       Serial.println(" ");
-  Serial.print("Gyro X: "); Serial.print((int)lsm.gyroData.x);   Serial.println(" ");
-  Serial.print("Y: "); Serial.print((int)lsm.gyroData.y);        Serial.println(" ");
-  Serial.print("Z: "); Serial.println((int)lsm.gyroData.z);      Serial.println(" ");
-  Serial.print("Temp: "); Serial.print((int)lsm.temperature);    Serial.println(" ");
+  Serial.print("Accel X: "); Serial.print((int)lsm.accelData.x);   Serial.println(" ");
+  Serial.print("Y: ");       Serial.print((int)lsm.accelData.y);   Serial.println(" ");
+  Serial.print("Z: ");       Serial.println((int)lsm.accelData.z); Serial.println(" ");
+  Serial.print("Mag X: ");   Serial.print((int)lsm.magData.x);     Serial.println(" ");
+  Serial.print("Y: ");       Serial.print((int)lsm.magData.y);     Serial.println(" ");
+  Serial.print("Z: ");       Serial.println((int)lsm.magData.z);   Serial.println(" ");
+  Serial.print("Gyro X: ");  Serial.print((int)lsm.gyroData.x);    Serial.println(" ");
+  Serial.print("Y: ");       Serial.print((int)lsm.gyroData.y);    Serial.println(" ");
+  Serial.print("Z: ");       Serial.println((int)lsm.gyroData.z);  Serial.println(" ");
+  Serial.print("Temp: ");    Serial.println((int)lsm.temperature); Serial.println(" "); Serial.println(" "); Serial.println(" ");
   delay(500);
 }
