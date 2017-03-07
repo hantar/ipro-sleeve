b43e1942a1156091ae4c9a1801660a9bb5a2f08e
hantar <hantar@hawk.iit.edu>
2017-03-06 21:17:26 -0600

    3D Visual Arduino Code - Need Processing IDE

--- rotate3D.ino
+++ rotate3D.ino
@@ -21,43 +21,32 @@ void configureLSM9DS0(void) {
   //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_6G);
   //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_8G);
   //lsm.setupAccel(lsm.LSM9DS0_ACCELRANGE_16G);
-  
   // 2.) Set the magnetometer sensitivity
   lsm.setupMag(lsm.LSM9DS0_MAGGAIN_2GAUSS);
   //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_4GAUSS);
   //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_8GAUSS);
   //lsm.setupMag(lsm.LSM9DS0_MAGGAIN_12GAUSS);
-
   // 3.) Setup the gyroscope
   lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_245DPS);
   //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_500DPS);
   //lsm.setupGyro(lsm.LSM9DS0_GYROSCALE_2000DPS);
 }
-
-void setup(void) 
-{
+void setup(void) {
   Serial.begin(115200);
   Serial.println(F("Adafruit LSM9DS0 9 DOF Board AHRS Example")); Serial.println("");
-  
   // Initialise the LSM9DS0 board.
-  if(!lsm.begin())
-  {
+  if (!lsm.begin()) {
     // There was a problem detecting the LSM9DS0 ... check your connections
     Serial.print(F("Ooops, no LSM9DS0 detected ... Check your wiring or I2C ADDR!"));
-    while(1);
+    while (1);
   }
-  
   // Setup the sensor gain and integration time.
   configureLSM9DS0();
 }
-
-void loop(void) 
-{
+void loop(void) {
   sensors_vec_t   orientation;
-
   // Use the simple AHRS function to get the current orientation.
-  if (ahrs.getOrientation(&orientation))
-  {
+  if (ahrs.getOrientation(&orientation)) {
     /* 'orientation' should have valid .roll and .pitch fields */
     Serial.print(F("Orientation: "));
     Serial.print(orientation.roll);
@@ -67,6 +56,5 @@ void loop(void)
     Serial.print(orientation.heading);
     Serial.println(F(""));
   }
-  
   delay(100);
 }
